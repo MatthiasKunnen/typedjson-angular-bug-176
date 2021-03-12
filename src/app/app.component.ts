0281143c31e42b538c4534babcabe479655e7cf3
Matthias Kunnen <matthias.kunnen@gmail.com>
2021-03-12 11:25:51 +0000

    Add test case

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {jsonMember, jsonObject, TypedJSON} from 'typedjson';
+
+
+@jsonObject
+class Empty {
+}
+
+@jsonObject
+class NotEmpty {
+
+    @jsonMember
+    dummy: string;
+}
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'typedjson-bug';
+
+    ngOnInit(): void {
+        console.log(TypedJSON.parse({foo: 'bar'}, Empty)); // e {}
+        console.log(TypedJSON.parse({foo: 'bar'}, NotEmpty)); // e {foo: "bar"}
+    }
 }
